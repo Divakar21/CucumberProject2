36a1ad6087a413d3ea371edfde1ba9b3f326766c
Divakar21 <55185206+Divakar21@users.noreply.github.com>
2019-10-18 18:14:15 +0530

    Update TestRunner1.java

--- src/test/java/com/demo/TestRunner/TestRunner1.java
+++ src/test/java/com/demo/TestRunner/TestRunner1.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src\\test\\resources\\Demoguru.feature", glue="com.demo.stepdefinition",dryRun=false,monochrome = true,
-plugin= {"pretty","html:target/cucumber-reports","rerun:target/failedScenarios.txt"},tags= {"@SmokeTest,@RegressionTest"})
+plugin= {"pretty","json:target/cucumber-reports/Cucumber.json","rerun:target/failedScenarios.txt"},tags= {"@SmokeTest,@RegressionTest"})
 
 public class TestRunner1 {
 
